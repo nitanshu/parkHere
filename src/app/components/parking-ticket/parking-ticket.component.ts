f5add3329bba42fb474960b6bfb799662c6b6c18
nitanshu <niverma@q3tech.com>
2018-02-08 17:46:30 +0530

    Parking Ticket is saved

--- src/app/components/parking-ticket/parking-ticket.component.ts
+++ src/app/components/parking-ticket/parking-ticket.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit, Input } from '@angular/core';
 import { ParkingSlot } from '../../models/parking-slot';
 import { ParkingTicket } from '../../models/parking-ticket';
 
+import { ParkingTicketService } from '../../services/parking-ticket.service';
+
 @Component({
   selector: 'app-parking-ticket',
   templateUrl: './parking-ticket.component.html',
@@ -11,10 +13,22 @@ import { ParkingTicket } from '../../models/parking-ticket';
 
 export class ParkingTicketComponent implements OnInit {
   @Input() parkingSlot: ParkingSlot;
+  parkingTickets: ParkingTicket[];
 
-  constructor() { }
+  constructor(private parkingTicketService: ParkingTicketService) { }
 
   ngOnInit() {
   }
-
+  add(parkingSlot: string, parking_slot_id: number, in_time: string, out_time: string, ticket_price: number): void {
+    in_time = in_time.trim();
+    out_time = out_time.trim();
+    if (!in_time) { return; }
+    this.parkingTicketService.bookTicket({ parking_slot_id, in_time, out_time, ticket_price } as ParkingTicket)
+      .subscribe(parkingTicket => {
+        this.parkingTickets.push(parkingTicket);
+      });
+  }
 }
+
+
+
--- src/app/services/parking-ticket.service.ts
+++ src/app/services/parking-ticket.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
+
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+
+import { ParkingTicket } from '../models/parking-ticket';
+
+const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) };
+
 @Injectable()
+
 export class ParkingTicketService {
-
-  constructor() { }
-
+  private parkingTicketUrl = 'http://localhost:3000/parking_tickets';
+  constructor(
+    private http: HttpClient
+  ) { }
+ bookTicket(parkingTicket: ParkingTicket): Observable<ParkingTicket> {
+   console.log('parking ticket object', parkingTicket);
+   return this.http.post<ParkingTicket>(this.parkingTicketUrl, parkingTicket, httpOptions);
+ }
 }
